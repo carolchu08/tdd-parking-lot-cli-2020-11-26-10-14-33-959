8a14f25c017c195a78dd0ed37f31d27a427086b2
CAROL CHU (OLP-ISD-OOCLL/HKG) <CHUCA2@OOCL.COM>
2020-11-27 15:33:30 +0800

    [parkingCar] [carol] feat: can generate a ticket when given two car with one capaciy parking lot

--- src/main/java/com/oocl/cultivation/ParkingLot.java
+++ src/main/java/com/oocl/cultivation/ParkingLot.java
@@ -12,6 +12,10 @@ public class ParkingLot {
     }
 
     public Ticket park(Car car) {
+        if(capacity<=0){
+            return null;
+        }
+        capacity--;
         return new Ticket();
     }
 }
