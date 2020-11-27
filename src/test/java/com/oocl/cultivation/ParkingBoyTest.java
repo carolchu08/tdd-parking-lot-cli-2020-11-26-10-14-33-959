3d481f503d6327ef90351882d15c3465362fffe5
CAROL CHU (OLP-ISD-OOCLL/HKG) <CHUCA2@OOCL.COM>
2020-11-27 15:10:59 +0800

    [parkingCar] [carol] feat: can return result of call function once when park the car

--- src/test/java/com/oocl/cultivation/ParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/ParkingBoyTest.java
@@ -2,9 +2,25 @@ package com.oocl.cultivation;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Calendar;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
 class ParkingBoyTest {
     @Test
-    void write_your_first_test() {
+    public void should_parking_boy_call_parking_lot_park_function_once_when_park_the_car() {
+        //given
+        ParkingLot parkingLot = new ParkingLot();
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
+        Car car = new Car();
+
+        //when
+        Ticket ticket = parkingBoy.park(car);
+
+        //then
+        assertNotNull(ticket);
 
     }
+
 }
